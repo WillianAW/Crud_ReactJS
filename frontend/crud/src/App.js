ba25cca660d0b512c7977e8bbecb934e378c6449
Willian Eduardo <willianeduardoangeliwolff@outlook.com>
2023-01-24 18:00:24 -0300

    criando interface

---
+++ frontend/crud/src/App.js
@@ -0,0 +1,28 @@
+
+import {toast, ToastContainer} from "react-toastify"; 
+import styled from "styled-components"
+import "react-toastify/dist/ReactToastify.css"
+import './App.css';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: felx;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+
+function App() {
+  return (
+    <div>
+      <Container>
+        
+      </Container>
+    </div>
+  );
+}
+
+export default App;
